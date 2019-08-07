bb2bca9f224fb8e144cee71f011f00e0f2b0eb43
iTBoyer <itboyer@qq.com>
2019-08-07 21:27:32 +0800

    更命令脚本

--- install.sh
+++ install.sh
@@ -2,13 +2,10 @@
 #快捷键方式安装
 #需要手动设置
 #chsh -s /bin/zsh  #设置zsh为默认的shell命令
-
-root=`pwd`
-spaceemacs.d=${root}/Util/iSpacemacsd
-#sed -i '' "s#export ZSH=.*#export ZSH='$zshDIR'#g" $template
-cd ~/
-pwd
+spaceemacs=`pwd`
+echo ${spaceemacs}
 target=".spaceemacs.d"
-ln -fs ${spaceemacs.d} ${target}
+cd ~/
+ln -fs ${spaceemacs} ${target}
 
 
